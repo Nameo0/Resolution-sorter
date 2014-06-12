551519f22856038c64f27eda8fee16e5d2b36a49
Nameo0 <nameo0.lsk@gmail.com>
2014-06-12 02:29:30 -0700

    Did some stuff and also enabled the program to move files

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Resolution-sorter
 =================
 
 Creating a ruby file to sort pictures via resolution
+
+*Note to self*
+git rm <FILE#NAME>  # Win7 desktop to remove file
--- main.rb
+++ main.rb
@@ -6,4 +6,27 @@ def resolution_finder(image_link)
   FastImage.size(image_link)
 end
 
-puts resolution_finder("D:/win7Part/Downloads/Wallpaper/1920x1080/1399441664160.jpg")
+size = resolution_finder("D:/win7Part/Downloads/Wallpaper/2 be sorted/1399441664160.jpg")
+width = size[0]
+height = size[1]
+
+file_name = Pathname.new("D:/win7Part/Downloads/Wallpaper/2 be sorted/1399441664160.jpg").basename
+
+puts "height = #{height}"
+puts "width = #{width}"
+# puts Dir["D:/win7part/Downloads/Wallpaper/2 be sorted/*"].empty?
+
+#while true
+  if Dir["D:/win7part/Downloads/Wallpaper/2 be sorted/*"].empty?
+    puts "Directory is empty"
+  else
+    puts "Directory is stuffy"
+    if File.directory?("D:/win7Part/Downloads/Wallpaper/#{width}x#{height}")
+      puts "Files will be put into folder"
+      File.rename("D:/win7part/Downloads/Wallpaper/2 be sorted/#{file_name}", "D:/win7Part/Downloads/Wallpaper/#{width}x#{height}/#{file_name}")
+    else
+      puts "Folder will be made and files put in"
+    end    
+  end
+  #sleep 300
+#end
